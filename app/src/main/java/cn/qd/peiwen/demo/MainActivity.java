e529291e32ede3c20d728157ca061d7ed68b95a3
peiwen.liu <peiwen.liu@picovr.com>
2019-11-15 14:16:23 +0800

    use PWTools 1.1.2

--- app/src/main/java/cn/qd/peiwen/demo/MainActivity.java
+++ app/src/main/java/cn/qd/peiwen/demo/MainActivity.java
@@ -1,13 +1,8 @@
 package cn.qd.peiwen.demo;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import java.nio.ByteOrder;
 
 import androidx.appcompat.app.AppCompatActivity;
-import cn.qd.peiwen.pwtools.ByteUtils;
-import cn.qd.peiwen.pwtools.logger.PWLogger;
 
 public class MainActivity extends AppCompatActivity {
 
